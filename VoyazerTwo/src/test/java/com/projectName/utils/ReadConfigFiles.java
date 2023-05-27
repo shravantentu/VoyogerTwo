7cf0583416866b6d465fb1cef1582e7453007197
shravantentu <36913919+shravantentu@users.noreply.github.com>
2023-05-27 09:36:18 +0530

    added comm 1

--- VoyazerTwo/src/test/java/com/projectName/utils/ReadConfigFiles.java
+++ VoyazerTwo/src/test/java/com/projectName/utils/ReadConfigFiles.java
@@ -3,6 +3,7 @@ package test.java.com.projectName.utils;
 public class ReadConfigFiles {
 	
 	public static String filepath = System.getProperties()+"\\src\\test\\resources\\Exceldata.xlsx";
+	//Added some commments
 	
 
 	
